b7a1e956def597c6e3ae4d2c38977dca31a2eec6
designruss <designruss@gmail.com>
2023-08-18 21:46:38 +0300

    problem solving, bean comparison (@Scope("prototype"))

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,18 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext applicationContext =
+        ApplicationContext context =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hw1 =
+                (HelloWorld) context.getBean("helloworld");
+        HelloWorld hw2 =
+                (HelloWorld)context.getBean("helloworld");
+        System.out.println(hw1==hw2);
+        Cat cat1 =
+                context.getBean("cat",Cat.class);
+        Cat cat2 =
+                context.getBean("cat",Cat.class);
+        System.out.println(cat1==cat2);
+
     }
 }
